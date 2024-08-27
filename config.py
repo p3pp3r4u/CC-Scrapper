9cc96db3d8d0c7537bd65a6d171267563d053bc6
p3pp3r4u <p3pper4u@gmail.com>
2024-08-27 18:49:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 # Channel: https://t.me/itsSmartDev
 
 # Pyrogram setup
-API_ID = "123456"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
-SESSION_STRING = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # Replace this SESSION STRING with your actual SESSION_STRING
-BOT_TOKEN = "12345678:XXXXXXXXXXXXXX"  # Replace this BOT_TOKEN
+API_ID = "28851176"  # Replace this API ID with your actual API ID
+API_HASH = "15367115c22adac8e6bbbe91875c013f"  # Replace this API HASH with your actual API HASH
+SESSION_STRING = "AgFQbiwAT6w_TI0ob7gwgGBM7SGyBLUMx5u_XnCo1p3izhmXK6rYmFl9pfEzUn1fci6iYOwwnecDgNuUBnzdRz-1A7pB-cBCEx2oV7hbs6-vKJjqGD_bA6i5R2QU2_IAx5Qmg09XvCAVNl_oLAAipjhNTOUdMTKdxJBC5PjVzYpNSyfXynic5PL2ubDdgQf-fX2MD9POg0l4YnNqtDdiDrQrTvwLlXBK_N-cdBLW0x5qZOAw9s40Te4A8RaWVTyXP-bRy2fPSL9FwDDnDGALHiWosc7klDHg_qDw0P8aKCWq5GFmZKbLPhp9ROxTvle7HTGcBvZYe-AzqxzvmxV2B9s0O7vdJAAAAAFe3E6pAA"  # Replace this SESSION STRING with your actual SESSION_STRING
+BOT_TOKEN = "6955825725:AAE5Lv1a8MvMcqTM6_9YsNfqP8zJI6uNVEQ"  # Replace this BOT_TOKEN
 
 # Admin IDs
-ADMIN_IDS = [12345678, 12345678]
+ADMIN_IDS = [5886463657, 1446146615]
 
 # Limits
 DEFAULT_LIMIT = 10000 # Card Scrapping Limit For Everyone
